f6cdad97a5a9c6c51c29d064aa9f2b02b826c7f3
mikel-btw <angeloso74@hotmail.com>
2026-09-07 15:40:48 -0500

    docs: update hardware configuration

---
+++ config/hardware.py
@@ -0,0 +1,6 @@
+HARDWARE_MACHINES = {
+    "1": "MacBook Pro 2012 (Debian)",
+    "2": "Lenovo IdeaPad L340 (Void Linux)",
+    "3": "iMac 2017 (macOS)",
+    "4": "Dell Precision (Windows/Debian)",
+}
